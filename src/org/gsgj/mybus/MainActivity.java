a09b1eaaafced1d0251f920344795219fe414d13
herohe <herohe2@naver.com>
2013-02-19 00:26:38 +0900

    - HTML 긁어오기 구현 - BusInfo 클래스 구현

--- src/org/gsgj/mybus/MainActivity.java
+++ src/org/gsgj/mybus/MainActivity.java
@@ -1,19 +1,67 @@
 package org.gsgj.mybus;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class MainActivity extends Activity {
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+@SuppressLint("ShowToast") public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        String url = "http://m.gbis.go.kr/jsp/searchList.jsp?mode=isearch&searchKeyValue=22219";
+        String html = downloadURL(url);
+        
+        TextView textView = (TextView)findViewById(R.id.html);
+        textView.setText(html);
+        
     }
 
 
-    @Override
+    private String downloadURL(String addr) {
+		String doc = "";
+		
+		URL url;
+		try {
+			url = new URL(addr);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			
+			if (con != null) {
+				con.setConnectTimeout(10000);
+				con.setUseCaches(false);
+				
+				if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					for (;;) {
+						String line = br.readLine();
+						if (line == null) {
+							break;
+						}
+						doc += line + "\n";
+					}
+					br.close();
+				}
+				con.disconnect();
+			}
+		} catch (Exception e) {
+			Toast.makeText(this, "parsing error!", Toast.LENGTH_SHORT);
+		}
+		
+		return doc;
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
